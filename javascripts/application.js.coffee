85d2e2898da182de4e5ebf3131a5be48eed22606
Francky <kifer.mada@gmail.com>
2013-11-29 12:55:16 +0300

    adding submenu that display on expand for active item

--- javascripts/application.js.coffee
+++ javascripts/application.js.coffee
@@ -12,20 +12,41 @@ $ ->
     hideMenuTexts: () ->
       $("nav.nbs-sidebar ul a span.expanded-element").hide()
 
+    showActiveSubMenu: () ->
+      $("li.active ul.nbs-level2").show()
+      $("li.active a.expandable").css({width: "100%"})
+
+    hideActiveSubMenu: () ->
+      $("li.active ul.nbs-level2").hide()
+
+    adjustPaddingOnExpand: () ->
+      $("ul.nbs-level1 li a.expandable").css({padding: "1px 4px 4px 0px"})
+      $("ul.nbs-level1 li.active a.expandable").css({padding: "1px 4px 4px 4px"})
+
+    resetOriginalPaddingOnCollapse: () ->
+      $("ul.nbs-level1 li a.expandable").css({padding: "4px 4px 4px 0px"})
+      $("ul.nbs-level1 li.active a.expandable").css({padding: "4px"})
+
     ignite: () ->
 
       ((nbs_instance) ->
         $("#justify-icon").click (e) ->
 
           if $(this).parent("nav.nbs-sidebar").hasClass("nbs-sidebar-menu-collapsed")
+            nbs_instance.adjustPaddingOnExpand()
+
             nbs_instance.expandMyMenu()
             nbs_instance.showMenuTexts()
+            nbs_instance.showActiveSubMenu()
 
             $(this).css({color: "#000"})
 
           else if $(this).parent("nav.nbs-sidebar").hasClass("nbs-sidebar-menu-expanded")
+            nbs_instance.resetOriginalPaddingOnCollapse()
+
             nbs_instance.collapseMyMenu()
             nbs_instance.hideMenuTexts()
+            nbs_instance.hideActiveSubMenu()
 
             $(this).css({color: "#FFF"})
 
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -20,18 +20,51 @@
         return $("nav.nbs-sidebar ul a span.expanded-element").hide();
       };
 
+      NBSEngine.prototype.showActiveSubMenu = function() {
+        $("li.active ul.nbs-level2").show();
+        return $("li.active a.expandable").css({
+          width: "100%"
+        });
+      };
+
+      NBSEngine.prototype.hideActiveSubMenu = function() {
+        return $("li.active ul.nbs-level2").hide();
+      };
+
+      NBSEngine.prototype.adjustPaddingOnExpand = function() {
+        $("ul.nbs-level1 li a.expandable").css({
+          padding: "1px 4px 4px 0px"
+        });
+        return $("ul.nbs-level1 li.active a.expandable").css({
+          padding: "1px 4px 4px 4px"
+        });
+      };
+
+      NBSEngine.prototype.resetOriginalPaddingOnCollapse = function() {
+        $("ul.nbs-level1 li a.expandable").css({
+          padding: "4px 4px 4px 0px"
+        });
+        return $("ul.nbs-level1 li.active a.expandable").css({
+          padding: "4px"
+        });
+      };
+
       NBSEngine.prototype.ignite = function() {
         return (function(nbs_instance) {
           return $("#justify-icon").click(function(e) {
             if ($(this).parent("nav.nbs-sidebar").hasClass("nbs-sidebar-menu-collapsed")) {
+              nbs_instance.adjustPaddingOnExpand();
               nbs_instance.expandMyMenu();
               nbs_instance.showMenuTexts();
+              nbs_instance.showActiveSubMenu();
               $(this).css({
                 color: "#000"
               });
             } else if ($(this).parent("nav.nbs-sidebar").hasClass("nbs-sidebar-menu-expanded")) {
+              nbs_instance.resetOriginalPaddingOnCollapse();
               nbs_instance.collapseMyMenu();
               nbs_instance.hideMenuTexts();
+              nbs_instance.hideActiveSubMenu();
               $(this).css({
                 color: "#FFF"
               });
